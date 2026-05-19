9f1539de27bdf3dc14c43315c16c4cc1a58b4cc8
madoka <madokalove@qq.com>
2026-05-19 21:42:47 +0800

    实现动画线

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
       :zIndex="item.zIndex"
       @mousedown="onClick(item)"
     />
+    <madoka-line position="top" direction="level" :move="-200" />
+    <madoka-line position="right" direction="vertical" :move="200"/>
+    <madoka-line position="bottom" direction="level" :move="-400" :default-pos="-200"/>
+    <madoka-line position="right" direction="vertical" :move="-400" :default-pos="-200"/>
   </div>
 </template>
 
@@ -13,6 +17,7 @@
 import MadokaViewSlice from "@/components/MadokaViewSlice.vue";
 import kami1 from "@/assets/images/picture-album/kami_1.webp";
 import kami5 from "@/assets/images/picture-album/kami_5.webp";
+import MadokaLine from "@/components/MadokaLine.vue";
 
 const onClick = (item) => {
   s.list.forEach((item) => {
---
+++ src/components/MadokaLine.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+      class="line"
+      :class="props.direction"
+      :style="s.style"
+  />
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+      direction: 'level' | 'vertical'
+      move: number
+      position: 'top' | 'left' | 'right' | 'bottom'
+      defaultPos?: number
+      delay?: number
+    }>(),
+    {
+
+      delay: 2000,
+      defaultPos: 100
+    }
+)
+
+const s = reactive({
+  style: {}
+})
+
+onMounted(() => {
+  s.style[props.position] = `${props.defaultPos}px`
+  setTimeout(() => {
+    if (props.direction === 'vertical') {
+      s.style.transform =
+          `translateX(${props.move}px)`
+      return
+    }
+    s.style.transform =
+        `translateY(${props.move}px)`
+
+  }, props.delay)
+})
+</script>
+
+<style scoped lang="less">
+.line {
+  position: fixed;
+  transition: transform 500ms ease-in-out;
+
+  &.level {
+
+    width: 100%;
+    height: 1px;
+
+    background: #000;
+  }
+
+  &.vertical {
+
+    width: 1px;
+    height: 100%;
+
+    background: #000;
+  }
+}
+</style>
